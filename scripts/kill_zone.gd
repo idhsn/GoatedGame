7d5ff87ffb0b1df65204bc50675f5cac6e598a30
IsOtI7 <pheonixthekira@gmail.com>
2025-07-12 17:18:48 +0100

    change

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -8,11 +8,6 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("die"):
 		body.die()  # This will trigger the death animation
 		timer.start()
-	else:
-		_restart_scene()
 
 func _on_timer_timeout() -> void:
-	_restart_scene()
-
-func _restart_scene():
 	get_tree().reload_current_scene()
