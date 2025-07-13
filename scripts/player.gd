4c2709addf04e67b1c7e2a7caf8374f23e84cbe3
idhsn <hatim.ibourki@gmail.com>
2025-07-13 23:03:30 +0100

    Pickable Items Shit

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,31 @@
 extends CharacterBody2D
 
+# Constants
 const SPEED = 150.0
 const JUMP_VELOCITY = -280.0
 
+# Nodes
 @onready var player_sprite: AnimatedSprite2D = $PlayerSprite
 
+# Player state
 var is_dead := false
+var has_weapon := false
+
+# Public methods
 func die():
 	is_dead = true
 	player_sprite.play("Death")
 
+func pickup_weapon():
+	has_weapon = true
+
 func _physics_process(delta: float) -> void:
 	if is_dead:
-		return  # Stop all movement if dead
+		return
 
-	# Add gravity
+	# Apply gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += get_gravity().y * delta
 
 	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -34,13 +43,28 @@ func _physics_process(delta: float) -> void:
 	# Play animations
 	if is_on_floor():
 		if direction == 0:
-			player_sprite.play("idle")
+			if has_weapon:
+				if player_sprite.animation != "Idle_with_GUNX":
+					player_sprite.play("Idle_with_GUNX")
+			else:
+				if player_sprite.animation != "idle":
+					player_sprite.play("idle")
 		else:
-			player_sprite.play("Run")
+			if has_weapon:
+				if player_sprite.animation != "Run_with_GUNX":
+					player_sprite.play("Run_with_GUNX")
+			else:
+				if player_sprite.animation != "Run":
+					player_sprite.play("Run")
 	else:
-		player_sprite.play("Jumpo")
+		if has_weapon:
+			if player_sprite.animation != "jump_with_weapon":
+				player_sprite.play("jump_with_weapon")
+		else:
+			if player_sprite.animation != "Jumpo":
+				player_sprite.play("Jumpo")
 
-	# Movement logic
+	# Horizontal movement
 	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
